bdb99071fe19a3840d9b91ae96fd5d7417bf5b03
emresennturk <combatia.fb.emre@hotmail.com>
2025-03-23 02:55:16 +0300

    Email sender update

--- src/main/java/com/Proje/Prisewise/services/PriceDropService.java
+++ src/main/java/com/Proje/Prisewise/services/PriceDropService.java
@@ -8,7 +8,6 @@ import jakarta.mail.internet.MimeMessage;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
